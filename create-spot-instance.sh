685185c6608f2b832cb850fc97351f9c66092d6b
lavanya <lavanya.bhadri>
2021-12-02 19:57:39 +0530

    create

--- create-spot-instance.sh
+++ create-spot-instance.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+UPDATE_DNS_RECORDS() {
+    IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+    ## xargs is used to remove the double  quotes
+    sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id Z0577679A6W027W86RBE --change-batch file:///tmp/record.json | jq  &>/dev/null
+}
+
 CREATE() {
   COUNT=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
-
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --launch-template LaunchTemplateId=lt-060bf846c161cbde6,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --launch-template LaunchTemplateId=lt-03d452b52cee20ce4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo -e "\e[1;33m$1 Instance already exists\e[0m"
+    UPDATE_DNS_RECORDS $1
     return
   fi
 
   sleep 5
 
-  IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
-  ## xargs is used to remove the double  quotes
-  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z07250483J27MWFJ6RU7C --change-batch file:///tmp/record.json | jq  &>/dev/null
+  UPDATE_DNS_RECORDS $1
 }
 
 if [ "$1" == "all" ]; then
@@ -28,3 +32,6 @@ if [ "$1" == "all" ]; then
 else
   CREATE $1
 fi
+
+
+
