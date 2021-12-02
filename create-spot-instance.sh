b88c057ced442e7cebee199d40ecb6c6d55c2a27
lavanya <lavanya.bhadri>
2021-12-02 15:39:37 +0530

    create

--- create-spot-instance.sh
+++ create-spot-instance.sh
@@ -16,7 +16,7 @@ CREATE() {
   IP=$(aws ec2 describe-instances --filters  "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
   ## xargs is used to remove the double  quotes
   sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z08537272MHAQMEL5KTIG --change-batch file:///tmp/record.json | jq  &>/dev/null
+  aws route53 change-resource-record-sets --hosted-zone-id Z07250483J27MWFJ6RU7C --change-batch file:///tmp/record.json | jq  &>/dev/null
 }
 
 if [ "$1" == "all" ]; then
